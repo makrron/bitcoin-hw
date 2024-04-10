6f061773b0df720bc4a9e39ec6f93e0c065b6d13
Makrron <mvazquezarenal@uma.es>
2024-04-10 14:31:14 +0200

    minor changes

--- main.c
+++ main.c
@@ -9,20 +9,18 @@ int main() {
 
     const char* private_key_hex = generate_private_key();
     if (private_key_hex != NULL) {
-        printf("Clave privada en hexadecimal: %s\n", private_key_hex);
+        printf("Hex Pricate Key: %s\n", private_key_hex);
 
         unsigned char public_key[33]; // Tamaño para clave pública comprimida
 
         char *wif_private_key = NULL;
         wif_private_key = convert_private_key_to_wif(private_key_hex);
-        printf("Private Key: %s\n", wif_private_key);
-
-
+        printf("[TESNET] WIF Private Key: %s\n", wif_private_key);
 
         size_t public_key_len = sizeof(public_key);
 
         if (generate_public_key_from_hex(private_key_hex, public_key, &public_key_len)) {
-            printf("Clave pública: ");
+            printf("[COMPRESSED] Public Key: ");
             for (size_t i = 0; i < public_key_len; i++) {
                 printf("%02x", public_key[i]);
             }
